4c7e6c531fd150b583daefe54be86bb2d5d07f4c
unknown <truongsteven107@gmail.com>
2022-02-27 23:58:49 -0800

    manifest parser

--- UI.py
+++ UI.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from datetime import date, datetime
+from typing import List
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QLabel
@@ -22,6 +23,47 @@ def log(description : str, isDev: bool=False):
     f = open("{0}/{1}".format(subdirectory, logfilename), "a")
     f.write("[{0}] [{1}] {2}\n".format(current_date, current_time, description))
     f.close()
+    
+
+def parseManifest(manifest_filename : str) -> List:
+
+    if not os.path.isfile("{0}\{1}".format(os.getcwd(), manifest_filename)):
+        log("parseManifest(): Failed to locate {0}\{1}".format(os.getcwd(), manifest_filename), isDev=True)
+        return False
+
+    f = open(manifest_filename, 'r')
+    containers = [] # e.g. [ (01, 01), 6, "Olive Oil"]
+    current_line_number = 0
+
+    for line in f:
+        if len(line) < 18: break
+        if line[0] != '[': break
+        if not line[1:3].isnumeric() or int(line[1:3]) > 8: break
+        if line[3] != ',': break
+        if not line[4:6].isnumeric() or int(line[1:3]) > 12: break
+        if line[6] != ']': break
+        if line[7] != ',': break
+        if line[8] != ' ': break
+        if line[9] != '{': break
+        if not line[10:15].isnumeric(): break
+        if line[15] != '}': break
+        if line[16] != ',': break
+        if line[17] != ' ': break
+
+        container_position = ( int(line[1:3]), int(line[4:6]) )
+        container_weight = int(line[10:15])
+        container_description = line[18:][:-1]
+
+        containers.append([container_position, container_weight, container_description])
+
+        current_line_number += 1
+
+    if current_line_number < 96:
+        log("parseManifest(): Error in manifest file on line {0}".format(current_line_number+1), isDev=True)
+        return False
+
+    return containers
+ 
 
 if __name__ == "__main__":
 
