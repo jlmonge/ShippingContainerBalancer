7967fbbe339ac184bf78b33c7f2ae975fe336d2f
Joseph Monge <josephmonge00@gmail.com>
2022-03-06 12:57:27 -0800

    Added navigation between pages

--- UI.py
+++ UI.py
@@ -1,105 +1,133 @@
 import sys
 from PyQt5.QtWidgets import (
-    QApplication,
-    QLabel,
-    QMainWindow,
-    QStatusBar,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-    QGridLayout,
-    QFormLayout,
-    QLineEdit,
-    QStackedLayout
+   QApplication,
+   QLabel,
+   QMainWindow,
+   QStatusBar,
+   QPushButton,
+   QVBoxLayout,
+   QWidget,
+   QGridLayout,
+   QFormLayout,
+   QLineEdit,
+   QStackedWidget,
+   QHBoxLayout,
+   QRadioButton,
+   QMessageBox,
+   QSizePolicy
 )
-
+ 
 from PyQt5.QtGui import QIcon
-
+ 
 '''
 PyQt5 install instructions
-
+ 
 (optional) 1. python3 -m venv pyqtenv
 (optional) 2. source pyqtenv/bin/activate
 3. pip3 install pyqt5
 '''
-
+ 
 '''
 window.setGeometry(100, 100, 1600, 900)
 window.move(60, 15)
 helloMsg = QLabel('Login', parent=window)
 '''
-
-
-
-class Window(QMainWindow):
+ 
+ 
+ 
+class UI(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+ 
+        # window config
         self.title = "Keogh's Yard Portage"
         self.width = 1024
         self.height = 576
         self.setWindowTitle(self.title)
         self.setFixedSize(self.width, self.height)
         self.setWindowIcon(QIcon("icon.png"))
-        self.widget = QWidget(self)
-        self.grid = QGridLayout()
-        self.setCentralWidget(self.widget)
-        self.widget.setLayout(self.grid)
-        '''
-        self.widgets = {
-            "login1": [],
-            "login2": [],
-            "name": [],
-            "balance": [],
-            "load": [],
-            "continue": [],
-            "back": [],
-            "run": [],
-            "unloadText": [],
-            "unloadGrid": [],
-            "loadText": [],
-            "add": [],
-            "complete": [],
-            "animation": [],
-            "instruction": []
-        }
-        '''
-        self.menuWidget()
+       
+        # pages config
+        self.loginPage = QWidget()
+        self.menuPage = QWidget()
+        self.loadPage = QWidget()
+        self.progressPage = QWidget()
+        self.animationPage = QWidget()
+        self.widgetStack = QStackedWidget(self)
+        self.widgetStack.addWidget(self.loginPage)
+        self.widgetStack.addWidget(self.menuPage)
+        self.widgetStack.addWidget(self.loadPage)
+        self.widgetStack.addWidget(self.progressPage)
+        self.widgetStack.addWidget(self.animationPage)
+   
+        self.loginFunc()
+ 
+       #start at the login page
 
-    '''
-    def clearWidgets(self):
-        for w in self.widgets:
-            if self.widgets[w] != []:
-                self.widgets[w][-1].hide()
-            for i in range(0, len(self.widgets[w])):
-                self.widgets[w].pop()
-    '''
-
-    
-    def loginWidget(self):
-        #self.clearWidgets()
+    def loginFunc(self):
+        print("login test")
+        self.widgetStack.setCurrentIndex(0)
         layout = QFormLayout()
         layout.addRow("Name",QLineEdit())
-        layout.addRow("Address",QLineEdit())
-        #self.setTabText(0,"Contact Details")
-        return 
-    
-    def menuWidget(self):
         loginBtn = QPushButton('Login')
-        loginBtn.clicked.connect(self.loginWidget)
+        loginBtn.setStyleSheet("min-width: 10em;")
+        layout.addWidget(loginBtn)
+ 
+        loginBtn.clicked.connect(self.menuFunc)
+        self.loginPage.setLayout(layout)
+ 
+    def menuFunc(self):
+        self.widgetStack.setCurrentIndex(1)
+        layout = QGridLayout()
+        loginBtn = QPushButton('Login')
+        loginBtn.clicked.connect(self.loginFunc)
         balanceBtn = QPushButton('Balance')
+        balanceBtn.clicked.connect(self.progressFunc)
         loadBtn = QPushButton('Load/Unload')
+        loadBtn.clicked.connect(self.loadFunc)
         contBtn = QPushButton('Continue')
+        contBtn.clicked.connect(self.animationFunc)
 
-        self.grid.addWidget(loginBtn)
-        self.grid.addWidget(balanceBtn)
-        self.grid.addWidget(loadBtn)
-        self.grid.addWidget(contBtn)
+        #'''
+        loginBtn.setStyleSheet("min-width: 20em;")
+        balanceBtn.setStyleSheet("min-width: 20em;")
+        loadBtn.setStyleSheet("min-width: 20em;")
+        contBtn.setStyleSheet("min-width: 20em;")
+        #'''
+   
+        layout.addWidget(loginBtn)
+        layout.addWidget(balanceBtn)
+        layout.addWidget(loadBtn)
+        layout.addWidget(contBtn)
+ 
+        self.menuPage.setLayout(layout)
+   
+    def loadFunc(self):
+        self.widgetStack.setCurrentIndex(2)
+        layout = QGridLayout()
+        testBtn = QPushButton('load')
+        testBtn.clicked.connect(self.loginFunc)
+        layout.addWidget(testBtn)
+        self.loadPage.setLayout(layout)
+   
+    def progressFunc(self):
+        self.widgetStack.setCurrentIndex(3)
+        layout = QGridLayout()
+        testBtn = QPushButton('progress')
+        testBtn.clicked.connect(self.loginFunc)
+        layout.addWidget(testBtn)
+        self.progressPage.setLayout(layout)
 
-
-
-
+    def animationFunc(self):
+        self.widgetStack.setCurrentIndex(4)
+        layout = QGridLayout()
+        testBtn = QPushButton('test')
+        testBtn.clicked.connect(self.loginFunc)
+        layout.addWidget(testBtn)
+        self.animationPage.setLayout(layout)
+ 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    win = Window()
-    win.show()
-    sys.exit(app.exec_())
+   app = QApplication(sys.argv)
+   ui = UI()
+   ui.show()
+   sys.exit(app.exec_())
