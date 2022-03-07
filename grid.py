e54214ed585d17684f90ab62d38d55ee023a40a1
unknown <truongsteven107@gmail.com>
2022-03-06 17:56:19 -0800

    Grid highlight + selection

--- grid.py
+++ grid.py
@@ -1,33 +1,73 @@
+from email.charset import QP
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
+from PyQt5.QtGui import QPainter, QColor, QPen, QPalette
 from util import *
 import sys
 
 
 class Grid(QTableWidget):
 
-    def __init__(self):
+    def __init__(self, containers):
         super().__init__()
 
-        ROW_SIZE = 8
-        COL_SIZE = 12
+        self.ROW_SIZE = 8
+        self.COL_SIZE = 12
+        self.SELECTION_COLOR = QColor(255,228,181)
+        self.containers = containers
+        self.selectedContainers = set()
 
-        containers = parseManifest("manifest.txt")
+        self.setRowCount(self.ROW_SIZE)
+        self.setColumnCount(self.COL_SIZE)
+        self.setHorizontalHeaderLabels([ str(i).center(15, ' ') for i in range(1, self.COL_SIZE+1)])
+        self.setVerticalHeaderLabels([str(i).center(3, ' ') for i in range(self.ROW_SIZE, 0, -1)])
+        self.setFocusPolicy(QtCore.Qt.FocusPolicy.NoFocus) # diasbles border highlighting
+        self.setSelectionMode(QAbstractItemView.NoSelection)  # disables widget highlighting https://stackoverflow.com/questions/24973378/how-to-disable-selection-highlighting-in-a-qtablewidget
+        self.setEditTriggers(QAbstractItemView.NoEditTriggers) # disables editing of cells
+    
+        self.horizontalHeader().resizeSections(QHeaderView.ResizeToContents) #width of cell depends on width of header text
+        self.setStyleSheet("QTableWidget{border: 1px solid black; width: 200px}")
 
-        self.setRowCount(ROW_SIZE)
-        self.setColumnCount(COL_SIZE)
-        self.setHorizontalHeaderLabels([str(i) for i in range(1, COL_SIZE+1)])
-        self.setVerticalHeaderLabels([str(i) for i in range(ROW_SIZE, 0, -1)])
 
-        for container in containers:
+
+        self.itemClicked.connect(self.onClick)
+
+        for container in self.containers:
             item = QTableWidgetItem(container[2])
 
-            self.setItem(ROW_SIZE-container[0][0]-1+1, container[0][1]-1, item)
+            if container[2] == "NAN":
+                item.setBackground(QColor(101, 110, 140))
 
-       
-    def onCellSelection(row: int, col: int):
-        print(row, col)
-            
+            self.setItem(self.ROW_SIZE-container[0][0]-1+1, container[0][1]-1, item)
+
+
+    def onClick(self, item: QTableWidgetItem) -> None:
+
+        row = self.ROW_SIZE-item.row()
+        col = item.column()+1
+
+        if item.text() == "NAN": return
+
+        if (row, col) in self.selectedContainers:
+            item.setBackground(QColor(255,255,255,0)) #reset background to white
+            self.selectedContainers.remove((row, col))
+        else:
+            item.setBackground(self.SELECTION_COLOR)
+            self.selectedContainers.add((row, col))
+        
+        print(self.getSelectedContainers())
+    
+    def getSelectedContainers(self) -> List:
+
+        selected = []
+
+        for container in self.containers:
+            pos = (container[0][0], container[0][1])
+            if pos in self.selectedContainers:
+                selected.append(container)
+        
+        return selected
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -35,10 +75,9 @@ if __name__ == "__main__":
     window = QWidget()
     layout = QFormLayout()
 
-    layout.addRow(Grid())
-
+    layout.addRow(Grid(parseManifest("manifest.txt")))
     window.setLayout(layout)
-
+    window.resize(741, 288)
     window.show()
 
     sys.exit(app.exec_())
