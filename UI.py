4c4afbe8da639b7df180f1426f309ac642c6f8b3
Joseph Monge <josephmonge00@gmail.com>
2022-03-09 09:42:47 -0800

    Pre-merge commit

--- UI.py
+++ UI.py
@@ -13,7 +13,8 @@ from PyQt5.QtWidgets import (
    QStackedWidget,
    QHBoxLayout,
    QRadioButton,
-   QMessageBox
+   QMessageBox,
+   QFileDialog
 )
  
 from PyQt5.QtGui import QIcon
@@ -44,7 +45,6 @@ class UI(QWidget):
         self.height = 576
         self.setWindowTitle(self.title)
         self.setFixedSize(self.width, self.height)
-        self.setWindowIcon(QIcon("icon.png"))
        
         # pages config
         self.loginPage = QWidget()
@@ -66,57 +66,65 @@ class UI(QWidget):
        #start at the login page
  
     def loginFunc(self):
-        print("login test")
         self.widgetStack.setCurrentIndex(0)
-        layout = QFormLayout()
+        layout = QFormLayout(self.loginPage)
         layout.addRow("Name",QLineEdit())
         loginBtn = QPushButton('Login')
         layout.addWidget(loginBtn)
  
         loginBtn.clicked.connect(self.menuFunc)
-        self.loginPage.setLayout(layout)
  
     def menuFunc(self):
         self.widgetStack.setCurrentIndex(1)
-        layout = QGridLayout()
+        
+        layout = QGridLayout(self.menuPage)
         loginBtn = QPushButton('Login')
         loginBtn.clicked.connect(self.loginFunc)
         balanceBtn = QPushButton('Balance')
         balanceBtn.clicked.connect(self.progressFunc)
-        loadBtn = QPushButton('Load/Unload')
+        loadBtn = QPushButton('Onload/Offload')
         loadBtn.clicked.connect(self.loadFunc)
         contBtn = QPushButton('Continue')
         contBtn.clicked.connect(self.contFunc)
+
+        '''
+        loginBtn.setStyleSheet("min-height: 5em;")
+        balanceBtn.setStyleSheet("min-height: 5em;")
+        loadBtn.setStyleSheet("min-height: 5em;")
+        contBtn.setStyleSheet("min-height: 5em;")
+        '''
    
         layout.addWidget(loginBtn)
         layout.addWidget(balanceBtn)
         layout.addWidget(loadBtn)
         layout.addWidget(contBtn)
- 
-        self.menuPage.setLayout(layout)
-   
+    
     def loadFunc(self):
+        fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")
+        #positions = parseManifest(filename)
         self.widgetStack.setCurrentIndex(2)
-        layout = QGridLayout()
+        layout = QGridLayout(self.loadPage)
         testBtn = QPushButton('load')
         testBtn.clicked.connect(self.loginFunc)
         layout.addWidget(testBtn)
-        self.loadPage.setLayout(layout)
    
     def progressFunc(self):
+        fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")
+        #positions = parseManifest(filename)
         self.widgetStack.setCurrentIndex(3)
-        layout = QGridLayout()
+        layout = QGridLayout(self.progressPage)
         testBtn = QPushButton('progress')
         testBtn.clicked.connect(self.loginFunc)
         layout.addWidget(testBtn)
-        self.progressPage.setLayout(layout)
+
+    def animationFunc(self):
+        print("animation")
    
     def contFunc(self):
         self.widgetStack.setCurrentIndex(4)
         layout = QGridLayout()
         testBtn = QPushButton('test')
         testBtn.clicked.connect(self.loginFunc)
-
 
 
 if __name__ == "__main__":
@@ -134,70 +142,3 @@ if __name__ == "__main__":
         ui = UI()
         ui.show()
         sys.exit(app.exec_())
-
-'''
-class Window(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.title = "Keogh's Yard Portage"
-        self.width = 1024
-        self.height = 576
-        self.setWindowTitle(self.title)
-        self.setFixedSize(self.width, self.height)
-        self.setStyleSheet("QWidget {font: 16pt}") # change font size of all widgets
-        self.widget = QWidget(self)
-        self.grid = QGridLayout()
-        self.setCentralWidget(self.widget)
-        self.widget.setLayout(self.grid)
-        self.widgets = {
-            "login1": [],
-            "login2": [],
-            "name": [],
-            "balance": [],
-            "load": [],
-            "continue": [],
-            "back": [],
-            "run": [],
-            "unloadText": [],
-            "unloadGrid": [],
-            "loadText": [],
-            "add": [],
-            "complete": [],
-            "animation": [],
-            "instruction": []
-        }
-        self.loginWidget()
-
-    def clearWidgets(self):
-        for w in self.widgets:
-            if self.widgets[w] != []:
-                self.widgets[w][-1].hide()
-            for i in range(0, len(self.widgets[w])):
-                self.widgets[w].pop()
-
-    
-    def loginWidget(self):
-        #self.clearWidgets()
-        layout = QFormLayout()
-        layout.addRow("Name",QLineEdit())
-        layout.addRow("Address",QLineEdit())
-        #self.setTabText(0,"Contact Details")
-    
-    def menuWidget(self):
-        loginBtn = QPushButton('Login')
-        loginBtn.clicked.connect(self.loginWidget)
-        balanceBtn = QPushButton('Balance')
-        loadBtn = QPushButton('Onload/Offload')
-        contBtn = QPushButton('Continue')
-
-        self.grid.addWidget(loginBtn)
-        self.grid.addWidget(balanceBtn)
-        self.grid.addWidget(loadBtn)
-        self.grid.addWidget(contBtn)
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    win = Window()
-    win.show()
-    sys.exit(app.exec_())
-'''
