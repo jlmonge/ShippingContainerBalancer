bc83729a0ecc2bd1469ba652c08e2083d92edc23
Joseph Monge <josephmonge00@gmail.com>
2022-03-15 16:53:11 -0700

    Add two new pages, and all buttons redirect to correct page

--- UI.py
+++ UI.py
@@ -2,6 +2,7 @@ from cgitb import text
 from sqlite3 import Time
 import sys
 from util import *
+import time
 
 from PyQt5.QtWidgets import (
    QApplication,
@@ -18,7 +19,8 @@ from PyQt5.QtWidgets import (
    QHBoxLayout,
    QRadioButton,
    QMessageBox,
-   QFileDialog
+   QFileDialog,
+   QProgressBar
 )
 
 from PyQt5.QtCore import Qt
@@ -52,43 +54,43 @@ class UI(QWidget):
         self.setWindowTitle(self.title)
         self.setFixedSize(self.width, self.height)
        
+        # determine if program was unexpectedly shutdown by power outage
+        self.cont = False
+
         # pages config
         self.loginPage = QWidget()
         self.menuPage = QWidget()
         self.loadPage = QWidget()
-        self.progressPage = QWidget()
+        self.calcPage = QWidget()
         self.animationPage = QWidget()
+        self.completePage = QWidget()
         self.widgetStack = QStackedWidget(self)
         self.widgetStack.setFixedSize(self.width, self.height)
         
-        self.widgetStack.addWidget(self.loginPage)
-        self.widgetStack.addWidget(self.menuPage)
-        self.widgetStack.addWidget(self.loadPage)
-        self.widgetStack.addWidget(self.progressPage)
-        self.widgetStack.addWidget(self.animationPage)
-
-        layout =  QVBoxLayout()
-        layout.addWidget(self.widgetStack)
-        self.setLayout(layout)
+        self.widgetStack.addWidget(self.loginPage)      #0
+        self.widgetStack.addWidget(self.menuPage)       #1
+        self.widgetStack.addWidget(self.loadPage)       #2
+        self.widgetStack.addWidget(self.calcPage)       #3
+        self.widgetStack.addWidget(self.animationPage)  #4
+        self.widgetStack.addWidget(self.completePage)   #5
 
         self.loginFunc()
  
        #start at the login page
  
+    def uploadHelper(self):
+        fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")[0]
+        print(fileName)
+        positions = parseManifest(fileName)
+        print(positions)
+
     def loginFunc(self):
         self.widgetStack.setCurrentIndex(0)
 
         layout = QFormLayout(self.loginPage)
-        layout.setAlignment(Qt.AlignCenter)
-        layout.setContentsMargins(370, 200, 0, 0)
-
         textfield = QLineEdit()
-        textfield.setMaximumWidth(200)
-        textfield.setMinimumHeight(25)
         layout.addRow("Name",textfield)
-
         loginBtn = QPushButton('Login')
-        loginBtn.setMaximumWidth(100)
         layout.addWidget(loginBtn)
 
         loginBtn.clicked.connect(self.menuFunc)
@@ -99,59 +101,83 @@ class UI(QWidget):
 
         loginBtn = QPushButton('Login')
         loginBtn.clicked.connect(self.loginFunc)
-        balanceBtn = QPushButton('Balance')
-        balanceBtn.clicked.connect(self.progressFunc)
         loadBtn = QPushButton('Onload/Offload')
         loadBtn.clicked.connect(self.loadFunc)
+        balanceBtn = QPushButton('Balance')
+        balanceBtn.clicked.connect(lambda: self.calcFunc(jobType=0))
         contBtn = QPushButton('Continue')
-        contBtn.clicked.connect(self.contFunc)
-
+        contBtn.clicked.connect(lambda: self.animationFunc(cont=self.cont))
         '''
-        loginBtn.setStyleSheet("min-height: 5em;")
-        balanceBtn.setStyleSheet("min-height: 5em;")
-        loadBtn.setStyleSheet("min-height: 5em;")
-        contBtn.setStyleSheet("min-height: 5em;")
+        loginBtn.setStyleSheet("padding: 5em;")
+        balanceBtn.setStyleSheet("padding: 5em;")
+        loadBtn.setStyleSheet("padding: 5em;")
+        contBtn.setStyleSheet("padding: 5em;")
         '''
-
-        loginBtn.setMaximumWidth(100)
-        balanceBtn.setMaximumWidth(100)
-        loadBtn.setMaximumWidth(100)
-        contBtn.setMaximumWidth(100)
-        loginBtn.setContentsMargins(0, 5, 0, 0)
-        layout.setContentsMargins(360,180,0,0)
-   
+        
         layout.addWidget(loginBtn)
-        layout.addWidget(balanceBtn)
         layout.addWidget(loadBtn)
+        layout.addWidget(balanceBtn)
         layout.addWidget(contBtn)
 
     def loadFunc(self):
-        fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")
-        #positions = parseManifest(filename)
         self.widgetStack.setCurrentIndex(2)
+        self.uploadHelper()
         layout = QGridLayout(self.loadPage)
-        testBtn = QPushButton('load')
-        testBtn.clicked.connect(self.loginFunc)
-        layout.addWidget(testBtn)
-   
-    def progressFunc(self):
-        fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")
-        #positions = parseManifest(filename)
-        self.widgetStack.setCurrentIndex(3)
-        layout = QGridLayout(self.progressPage)
-        testBtn = QPushButton('progress')
-        testBtn.clicked.connect(self.loginFunc)
+        testBtn = QPushButton('Run')
+        testBtn.clicked.connect(lambda: self.animationFunc(cont=self.cont))
         layout.addWidget(testBtn)
 
-    def animationFunc(self):
-        print("animation")
-   
-    def contFunc(self):
+
+    '''
+    Input: job type (0 for unload/offload, 1 for balance)
+    '''
+    def calcFunc(self, jobType):
+        self.widgetStack.setCurrentIndex(3)
+        fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")
+        #positions = parseManifest(filename)
+        layout = QVBoxLayout(self.calcPage)
+
+        progLabel = QLabel(self.calcPage)
+        progLabel.setText('Calculating...')
+        progLabel.setAlignment(Qt.AlignCenter)
+        layout.addWidget(progLabel)
+
+        progBar = QProgressBar(self.calcPage)
+        maxProg = 100
+        progBar.setMaximum(maxProg)
+        layout.addWidget(progBar)
+
+        testBtn = QPushButton('Proceed')
+        testBtn.clicked.connect(lambda: self.animationFunc(cont=self.cont))
+        testBtn.setEnabled(False)
+        layout.addWidget(testBtn)
+
+        for i in range(maxProg):
+            time.sleep(0.01)
+            progBar.setValue(i+1)
+            QApplication.processEvents()
+        testBtn.setEnabled(True)
+            
+
+    def animationFunc(self, cont):
         self.widgetStack.setCurrentIndex(4)
         layout = QGridLayout(self.animationPage)
-        testBtn = QPushButton('test')
-        testBtn.clicked.connect(self.loginFunc)
+        testBtn = QPushButton('Complete operation')
+        testBtn.clicked.connect(self.completeFunc)
         layout.addWidget(testBtn)
+
+    def completeFunc(self):
+        self.widgetStack.setCurrentIndex(5)
+        layout = QGridLayout(self.completePage)
+        
+        progLabel = QLabel(self.completePage)
+        progLabel.setText('All operations have been completed')
+        progLabel.setAlignment(Qt.AlignCenter)
+        layout.addWidget(progLabel)
+
+        okBtn = QPushButton('OK')
+        okBtn.clicked.connect(self.menuFunc)
+        layout.addWidget(okBtn)
 
 
 if __name__ == "__main__":
