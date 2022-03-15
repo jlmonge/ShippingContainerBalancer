49a42f5cb7c5127c6ccbdcc35ba62809daa3f054
Garrett Pedvin <gsternumber2@gmail.com>
2022-03-15 02:42:35 -0700

    basic functionality of onload page. list of containers disappears after reloading the layout, and grid still needs to be applied.

--- UI.py
+++ UI.py
@@ -18,11 +18,20 @@ from PyQt5.QtWidgets import (
    QHBoxLayout,
    QRadioButton,
    QMessageBox,
+   QScrollArea,
+   QButtonGroup,
+   QMessageBox,
+   QSizePolicy,
+   QInputDialog,
+   QPlainTextEdit,
+   QListWidget,
+   QListWidgetItem,
+   QListView,
    QFileDialog
 )
 
 from PyQt5.QtCore import Qt
- 
+
 from PyQt5.QtGui import QIcon
  
 '''
@@ -39,9 +48,13 @@ window.move(60, 15)
 helloMsg = QLabel('Login', parent=window)
 '''
  
- 
+app = QApplication(sys.argv)
  
 class UI(QWidget):
+
+    #keep him global for use in other functions
+    loadRScrollBox = QListWidget()
+    
     def __init__(self, parent=None):
         super().__init__(parent)
  
@@ -74,7 +87,19 @@ class UI(QWidget):
         self.loginFunc()
  
        #start at the login page
- 
+
+    def menuConfirmPopup(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Confirmation")
+        msg.setText("Are you sure you want to go back to the main menu?")
+        msg.setIcon(QMessageBox.Information)
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        msg.setDefaultButton(QMessageBox.Cancel)
+        msg.setEscapeButton(QMessageBox.Cancel)
+        if (msg.exec_() == 1024): #1024 = QMessageBox.OK
+            #print("Back to menu", flush=True)
+            self.menuFunc()
+        
     def loginFunc(self):
         self.widgetStack.setCurrentIndex(0)
 
@@ -125,14 +150,107 @@ class UI(QWidget):
         layout.addWidget(loadBtn)
         layout.addWidget(contBtn)
 
+    #globals to be used by load page
+    onloadListNames = []
+    onloadListWts = []
+
+    def computeConfirmPopup(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Confirmation")
+        msg.setText("Are you sure you want to begin computing the sequence of moves? You will be unable to make any changes when you do.")
+        msg.setIcon(QMessageBox.Information)
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        msg.setDefaultButton(QMessageBox.Cancel)
+        msg.setEscapeButton(QMessageBox.Cancel)
+        if (msg.exec_() == 1024):
+            print("BEGINNING COMPUTATION", flush = True)
+            #--------------------------------------------------
+            #TODO: Pass to AI
+            #TODO: To operation-list page
+            #--------------------------------------------------
+
+    def addToOnloadList(self):
+        cNm, okPressed = QInputDialog.getText(self, "Onload Container", "Enter container contents:")
+        if (okPressed):
+            cWt, okPressed = QInputDialog.getInt(self, "Onload Container", "Enter container weight:", 0, 0, 5000, 1)
+        if (okPressed):
+            self.onloadListNames.append(str(cNm))
+            self.onloadListWts.append(cWt)
+            item = QListWidgetItem()
+            item.setText(str(self.onloadListNames.index(cNm) + 1) + ". " + cNm + "   Wt = " + str(cWt))
+            self.loadRScrollBox.addItem(item)
+
+    def removeFromOnloadList(self):
+            currItem = self.loadRScrollBox.currentItem()
+            msg2 = QMessageBox()
+            msg2.setWindowTitle("Confirmation")
+            msg2.setText("Are you sure you want to remove the following container from the onload list?\n" + currItem.text())
+            msg2.setIcon(QMessageBox.Information)
+            msg2.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            msg2.setDefaultButton(QMessageBox.Cancel)
+            msg2.setEscapeButton(QMessageBox.Cancel)
+            if (msg2.exec_() == 1024):
+                ind = int(currItem.text()[0]) - 1
+                self.onloadListNames.remove(self.onloadListNames[ind])
+                self.onloadListWts.remove(self.onloadListWts[ind])
+                gbye = self.loadRScrollBox.takeItem(ind)
+                del gbye
+                #print("Removed!", flush = True)
+                for i in range (0, len(self.onloadListNames)):
+                    self.loadRScrollBox.setCurrentRow(i)
+                    self.loadRScrollBox.currentItem().setText(str(i+1) + ". " + self.onloadListNames[i] + "   Wt = " + str(self.onloadListWts[i]))
+                #print("List refreshed!", flush = True)
+
     def loadFunc(self):
-        fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")
+        #fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")
         #positions = parseManifest(filename)
         self.widgetStack.setCurrentIndex(2)
-        layout = QGridLayout(self.loadPage)
-        testBtn = QPushButton('load')
-        testBtn.clicked.connect(self.loginFunc)
-        layout.addWidget(testBtn)
+        layout = QHBoxLayout(self.loadPage)
+        
+        theLeft = QVBoxLayout() #login, back, compute buttons
+        theLeft.addWidget(QWidget(), 4)
+        loginBtn = QPushButton('Login')
+        loginBtn.clicked.connect(self.loginFunc)
+        theLeft.addWidget(loginBtn, 1)
+        backBtn = QPushButton('Back to Menu')
+        backBtn.clicked.connect(self.menuConfirmPopup)
+        theLeft.addWidget(backBtn, 1)
+        theLeft.addWidget(QWidget(), 3)
+        computeBtn = QPushButton('Compute Solution')
+        computeBtn.clicked.connect(self.computeConfirmPopup)
+        theLeft.addWidget(computeBtn, 1)
+        theLeft.addWidget(QWidget(), 10)
+        
+        theCenter = QVBoxLayout() #grid and title of page
+        theCenter.addWidget(QLabel("Unload"), 1, Qt.AlignHCenter)
+        theGrid = QGridLayout()
+        #Get grid from grid.py
+        theCenter.addLayout(theGrid, 18)
+        theCenter.addWidget(QWidget(), 1)
+        
+        theRight = QVBoxLayout() #name and containers to onload
+        theRight.addWidget(QLabel("Hello, User!\nNot you? Please log in"), 2)
+        addOnlBtn = QPushButton('Add container to onload...')
+        addOnlBtn.clicked.connect(self.addToOnloadList)
+        theRight.addWidget(addOnlBtn, 4)
+        theScrollArea = QScrollArea()
+        theScrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        theScrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        theScrollLayout = QVBoxLayout(theScrollArea)
+        theScrollArea.setWidget(theScrollLayout.widget())
+        loadList = self.loadRScrollBox
+        loadList.itemClicked.connect(self.removeFromOnloadList)
+        theScrollLayout.addWidget(loadList)
+        loadList.update()
+        theRight.addWidget(theScrollArea, 12)
+        theRight.addWidget(QWidget(), 2)
+
+        layout.addLayout(theLeft, 1)
+        layout.addLayout(theCenter, 5)
+        layout.addLayout(theRight, 2)
+        layout.setSpacing(10)
+
+        self.setLayout(layout)
    
     def progressFunc(self):
         fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")
@@ -155,8 +273,6 @@ class UI(QWidget):
 
 
 if __name__ == "__main__":
-
-    app = QApplication(sys.argv)
 
     if not sys.platform.startswith('win32'):
         msgbox = QMessageBox()
