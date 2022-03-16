4b6cce764632731a0bb25e56a6f2aa2476c6fb08
Joseph Monge <josephmonge00@gmail.com>
2022-03-15 22:25:01 -0700

    Refactor init and add name functionality

--- UI.py
+++ UI.py
@@ -58,12 +58,64 @@ class UI(QWidget):
         self.cont = False
 
         # pages config
-        self.loginPage = QWidget()
-        self.menuPage = QWidget()
-        self.loadPage = QWidget()
         self.calcPage = QWidget()
+        layout3 = QVBoxLayout(self.calcPage)
+        progLabel = QLabel(self.calcPage)
+        progLabel.setText('Calculating...')
+        progLabel.setAlignment(Qt.AlignCenter)
+        progBar = QProgressBar(self.calcPage)
+        progBar.setMaximum(100)
+        progBtn = QPushButton('Proceed')
+        progBtn.clicked.connect(lambda: self.animationFunc(cont=0))
+        layout3.addWidget(progLabel)
+        layout3.addWidget(progBar)
+        layout3.addWidget(progBtn)
+
+        self.loginPage = QWidget()
+        layout0 = QFormLayout(self.loginPage)
+        textfield = QLineEdit()
+        layout0.addRow("Name", textfield)
+        loginBtn = QPushButton('Login')
+        layout0.addWidget(loginBtn)
+        loginBtn.clicked.connect(lambda: self.getName(textfield))
+
+        self.menuPage = QWidget()
+        layout1 = QGridLayout(self.menuPage)
+        loginBtn = QPushButton('Login')
+        loginBtn.clicked.connect(self.loginFunc)
+        loadBtn = QPushButton('Onload/Offload')
+        loadBtn.clicked.connect(lambda: self.uploadHelper(jobType=0))
+        balanceBtn = QPushButton('Balance')
+        balanceBtn.clicked.connect(lambda: self.uploadHelper(jobType=1, progBar=progBar, progBtn=progBtn))
+        contBtn = QPushButton('Continue')
+        contBtn.clicked.connect(lambda: self.animationFunc(cont=self.cont))
+        layout1.addWidget(loginBtn)
+        layout1.addWidget(loadBtn)
+        layout1.addWidget(balanceBtn)
+        layout1.addWidget(contBtn)
+
+        self.loadPage = QWidget()
+        layout2 = QGridLayout(self.loadPage)
+        testBtn = QPushButton('Run')
+        testBtn.clicked.connect(lambda: self.calcFunc(jobType=0, progBar=progBar, progBtn=progBtn))
+        layout2.addWidget(testBtn)   
+
         self.animationPage = QWidget()
+        layout4 = QGridLayout(self.animationPage)
+        testBtn = QPushButton('Complete operation')
+        testBtn.clicked.connect(self.completeFunc)
+        layout4.addWidget(testBtn)
+
         self.completePage = QWidget()
+        layout5 = QGridLayout(self.completePage)
+        progLabel = QLabel(self.completePage)
+        progLabel.setText('All operations have been completed')
+        progLabel.setAlignment(Qt.AlignCenter)
+        okBtn = QPushButton('OK')
+        okBtn.clicked.connect(self.menuFunc)
+        layout5.addWidget(progLabel)
+        layout5.addWidget(okBtn)
+
         self.widgetStack = QStackedWidget(self)
         self.widgetStack.setFixedSize(self.width, self.height)
         
@@ -78,106 +130,52 @@ class UI(QWidget):
  
        #start at the login page
  
-    def uploadHelper(self):
+    def uploadHelper(self, jobType, progBar=None, progBtn=None):
         fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")[0]
-        print(fileName)
-        positions = parseManifest(fileName)
-        print(positions)
+        if fileName:
+            #positions = parseManifest(fileName)
+            #print(positions)
+            if jobType == 0:
+                self.loadFunc()
+            else:
+                self.calcFunc(jobType=jobType, progBar=progBar, progBtn=progBtn)
 
     def loginFunc(self):
         self.widgetStack.setCurrentIndex(0)
+    
+    def getName(self, textfield):
+        name = textfield.text()
+        self.setWindowTitle(self.title + f" ({name})")
 
-        layout = QFormLayout(self.loginPage)
-        textfield = QLineEdit()
-        layout.addRow("Name",textfield)
-        loginBtn = QPushButton('Login')
-        layout.addWidget(loginBtn)
-
-        loginBtn.clicked.connect(self.menuFunc)
+        self.menuFunc()
  
     def menuFunc(self):
         self.widgetStack.setCurrentIndex(1)
-        layout = QGridLayout(self.menuPage)
-
-        loginBtn = QPushButton('Login')
-        loginBtn.clicked.connect(self.loginFunc)
-        loadBtn = QPushButton('Onload/Offload')
-        loadBtn.clicked.connect(self.loadFunc)
-        balanceBtn = QPushButton('Balance')
-        balanceBtn.clicked.connect(lambda: self.calcFunc(jobType=0))
-        contBtn = QPushButton('Continue')
-        contBtn.clicked.connect(lambda: self.animationFunc(cont=self.cont))
-        '''
-        loginBtn.setStyleSheet("padding: 5em;")
-        balanceBtn.setStyleSheet("padding: 5em;")
-        loadBtn.setStyleSheet("padding: 5em;")
-        contBtn.setStyleSheet("padding: 5em;")
-        '''
-        
-        layout.addWidget(loginBtn)
-        layout.addWidget(loadBtn)
-        layout.addWidget(balanceBtn)
-        layout.addWidget(contBtn)
 
     def loadFunc(self):
         self.widgetStack.setCurrentIndex(2)
-        self.uploadHelper()
-        layout = QGridLayout(self.loadPage)
-        testBtn = QPushButton('Run')
-        testBtn.clicked.connect(lambda: self.animationFunc(cont=self.cont))
-        layout.addWidget(testBtn)
 
 
     '''
     Input: job type (0 for unload/offload, 1 for balance)
     '''
-    def calcFunc(self, jobType):
+    def calcFunc(self, jobType, progBar, progBtn):
         self.widgetStack.setCurrentIndex(3)
-        fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")
-        #positions = parseManifest(filename)
-        layout = QVBoxLayout(self.calcPage)
+        progBar.reset()
+        progBtn.setEnabled(False)
 
-        progLabel = QLabel(self.calcPage)
-        progLabel.setText('Calculating...')
-        progLabel.setAlignment(Qt.AlignCenter)
-        layout.addWidget(progLabel)
-
-        progBar = QProgressBar(self.calcPage)
-        maxProg = 100
-        progBar.setMaximum(maxProg)
-        layout.addWidget(progBar)
-
-        testBtn = QPushButton('Proceed')
-        testBtn.clicked.connect(lambda: self.animationFunc(cont=self.cont))
-        testBtn.setEnabled(False)
-        layout.addWidget(testBtn)
-
-        for i in range(maxProg):
+        for i in range(100):
             time.sleep(0.01)
             progBar.setValue(i+1)
             QApplication.processEvents()
-        testBtn.setEnabled(True)
+        progBtn.setEnabled(True)
             
 
     def animationFunc(self, cont):
         self.widgetStack.setCurrentIndex(4)
-        layout = QGridLayout(self.animationPage)
-        testBtn = QPushButton('Complete operation')
-        testBtn.clicked.connect(self.completeFunc)
-        layout.addWidget(testBtn)
 
     def completeFunc(self):
         self.widgetStack.setCurrentIndex(5)
-        layout = QGridLayout(self.completePage)
-        
-        progLabel = QLabel(self.completePage)
-        progLabel.setText('All operations have been completed')
-        progLabel.setAlignment(Qt.AlignCenter)
-        layout.addWidget(progLabel)
-
-        okBtn = QPushButton('OK')
-        okBtn.clicked.connect(self.menuFunc)
-        layout.addWidget(okBtn)
 
 
 if __name__ == "__main__":
