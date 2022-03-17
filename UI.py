da531b94c2f1a43404adf1ef846b7fcb6d0ffe9f
unknown <truongsteven107@gmail.com>
2022-03-16 21:30:37 -0700

    update

--- UI.py
+++ UI.py
@@ -3,6 +3,7 @@ from sqlite3 import Time
 import sys
 from util import *
 import time
+from grid import Grid
 
 from PyQt5.QtWidgets import (
    QApplication,
@@ -19,12 +20,21 @@ from PyQt5.QtWidgets import (
    QHBoxLayout,
    QRadioButton,
    QMessageBox,
+   QScrollArea,
+   QButtonGroup,
+   QMessageBox,
+   QSizePolicy,
+   QInputDialog,
+   QPlainTextEdit,
+   QListWidget,
+   QListWidgetItem,
+   QListView,
    QFileDialog,
    QProgressBar
 )
 
 from PyQt5.QtCore import Qt
- 
+
 from PyQt5.QtGui import QIcon
  
 '''
@@ -41,9 +51,20 @@ window.move(60, 15)
 helloMsg = QLabel('Login', parent=window)
 '''
  
- 
+app = QApplication(sys.argv)
  
 class UI(QWidget):
+
+    #keep him global for use in multiple functions
+    loadRScrollBox = QListWidget()
+
+    #bools! let's not reassign stuff
+    loginLayoutSet = False
+    menuLayoutSet = False
+    loadLayoutSet = False
+    progressLayoutSet = False
+    animationLayoutSet = False
+    
     def __init__(self, parent=None):
         super().__init__(parent)
  
@@ -95,10 +116,56 @@ class UI(QWidget):
         layout1.addWidget(contBtn)
 
         self.loadPage = QWidget()
-        layout2 = QGridLayout(self.loadPage)
-        testBtn = QPushButton('Run')
-        testBtn.clicked.connect(lambda: self.calcFunc(jobType=0, progBar=progBar, progBtn=progBtn))
-        layout2.addWidget(testBtn)   
+        layout2 = QHBoxLayout(self.loadPage)
+
+        theLeft = QVBoxLayout() #login, back, compute buttons
+        theLeft.addWidget(QWidget(), 4)
+        loginBtn = QPushButton('Login')
+        loginBtn.clicked.connect(self.loginFunc)
+        theLeft.addWidget(loginBtn, 1)
+        backBtn = QPushButton('Back to Menu')
+        backBtn.clicked.connect(self.menuConfirmPopup)
+        theLeft.addWidget(backBtn, 1)
+        theLeft.addWidget(QWidget(), 3)
+        computeBtn = QPushButton('Compute Solution')
+        computeBtn.clicked.connect(self.computeConfirmPopup)
+        theLeft.addWidget(computeBtn, 1)
+        theLeft.addWidget(QWidget(), 10)
+    
+        theCenter = QVBoxLayout() #grid and title of page
+        
+        unloadLabel = QLabel("Unload")
+        theCenter.addWidget(unloadLabel, Qt.AlignHCenter)
+        theCenter.addWidget(Grid(parseManifest("manifest.txt")))
+
+        theRight = QVBoxLayout() #name and containers to onload
+        theRight.addWidget(QLabel("Hello, User!\nNot you? Please log in"), 2)
+        addOnlBtn = QPushButton('Add container to onload...')
+        addOnlBtn.clicked.connect(self.addToOnloadList)
+        theRight.addWidget(addOnlBtn, 4)
+        theScrollArea = QScrollArea()
+        theScrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        theScrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        theScrollLayout = QVBoxLayout(theScrollArea)
+        theScrollArea.setWidget(theScrollLayout.widget())
+        loadList = self.loadRScrollBox
+        loadList.itemClicked.connect(self.removeFromOnloadList)
+        theScrollLayout.addWidget(loadList)
+        loadList.update()
+        theRight.addWidget(theScrollArea, 12)
+        theRight.addWidget(QWidget(), 2)
+
+        # testBtn = QPushButton('Run')
+        # testBtn.clicked.connect(lambda: self.calcFunc(jobType=0, progBar=progBar, progBtn=progBtn))
+        # layout2.addWidget(testBtn)   
+        # layout2.addWidget(grid)
+
+
+        layout2.addLayout(theLeft, 1)
+        layout2.addLayout(theCenter, 5)
+        layout2.addLayout(theRight, 2)
+        layout2.setSpacing(10)
+ 
 
         self.animationPage = QWidget()
         layout4 = QGridLayout(self.animationPage)
@@ -129,6 +196,88 @@ class UI(QWidget):
         self.loginFunc(textfield, loginBtn0)
  
        #start at the login page
+
+    def menuConfirmPopup(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Confirmation")
+        msg.setText("Are you sure you want to go back to the main menu?")
+        msg.setIcon(QMessageBox.Information)
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        msg.setDefaultButton(QMessageBox.Cancel)
+        msg.setEscapeButton(QMessageBox.Cancel)
+        if (msg.exec_() == 1024): #1024 = QMessageBox.OK
+            #print("Back to menu", flush=True)
+            self.menuFunc()
+        
+
+
+    def computeConfirmPopup(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Confirmation")
+        msg.setText("Are you sure you want to begin computing the sequence of moves? You will be unable to make any changes when you do.")
+        msg.setIcon(QMessageBox.Information)
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        msg.setDefaultButton(QMessageBox.Cancel)
+        msg.setEscapeButton(QMessageBox.Cancel)
+        if (msg.exec_() == 1024):
+            print("BEGINNING COMPUTATION", flush = True)
+            #--------------------------------------------------
+            #TODO: Pass to AI
+            #TODO: To operation-list page
+            #--------------------------------------------------
+
+    def addToOnloadList(self):
+        cNm, okPressed = QInputDialog.getText(self, "Onload Container", "Enter container contents:")
+        if (okPressed):
+            cWt, okPressed = QInputDialog.getInt(self, "Onload Container", "Enter container weight:", 0, 0, 5000, 1)
+        if (okPressed):
+            self.onloadListNames.append(str(cNm))
+            self.onloadListWts.append(cWt)
+            item = QListWidgetItem()
+            item.setText(str(self.onloadListNames.index(cNm) + 1) + ". " + cNm + "   Wt = " + str(cWt))
+            self.loadRScrollBox.addItem(item)
+
+    def removeFromOnloadList(self):
+            currItem = self.loadRScrollBox.currentItem()
+            msg2 = QMessageBox()
+            msg2.setWindowTitle("Confirmation")
+            msg2.setText("Are you sure you want to remove the following container from the onload list?\n" + currItem.text())
+            msg2.setIcon(QMessageBox.Information)
+            msg2.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            msg2.setDefaultButton(QMessageBox.Cancel)
+            msg2.setEscapeButton(QMessageBox.Cancel)
+            if (msg2.exec_() == 1024):
+                ind = int(currItem.text()[0]) - 1
+                self.onloadListNames.remove(self.onloadListNames[ind])
+                self.onloadListWts.remove(self.onloadListWts[ind])
+                gbye = self.loadRScrollBox.takeItem(ind)
+                del gbye
+                #print("Removed!", flush = True)
+                for i in range (0, len(self.onloadListNames)):
+                    self.loadRScrollBox.setCurrentRow(i)
+                    self.loadRScrollBox.currentItem().setText(str(i+1) + ". " + self.onloadListNames[i] + "   Wt = " + str(self.onloadListWts[i]))
+                #print("List refreshed!", flush = True)
+
+
+    def progressFunc(self):
+        fileName = QFileDialog.getOpenFileName(self, "Open File", os.getenv('HOME'), "Text files (*.txt)")
+        containers = parseManifest(fileName[0].rsplit('/', 1)[1])
+
+        self.widgetStack.setCurrentIndex(3)
+        if (self.progressLayoutSet == False):
+            layout = QGridLayout(self.progressPage)
+            testBtn = QPushButton('progress')
+            testBtn.clicked.connect(self.loginFunc)
+            layout.addWidget(testBtn)
+            self.progressLayoutSet == True
+
+    def animationFunc(self):
+        if (self.animationLayoutSet == False):
+            print("animation", flush = True)
+            self.animationLayoutSet == True
+   
+    def contFunc(self):
+        pass
  
     def uploadHelper(self, jobType, progBar=None, progBtn=None):
         fileName = QFileDialog.getOpenFileName(self, "Open File", "C:\\", "Text files (*.txt)")[0]
@@ -176,6 +325,7 @@ class UI(QWidget):
             
 
     def animationFunc(self, cont):
+
         self.widgetStack.setCurrentIndex(4)
 
     def completeFunc(self):
@@ -183,8 +333,6 @@ class UI(QWidget):
 
 
 if __name__ == "__main__":
-
-    app = QApplication(sys.argv)
 
     if not sys.platform.startswith('win32'):
         msgbox = QMessageBox()
